eb182a480b0aae26e6af55001f225b0b59436f9a
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-12-28 19:10:26 -0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
 	return (
 		<div className='App'>
 			<img src={logo} alt='logo'/>
-			<h1>Website under construction...</h1>
+			<h1>Site under construction...</h1>
 		</div>
 	);
 };
